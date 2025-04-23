cc25e8894882b4ad096d1cadc3ad800352fce475
Jed May* <Jed.May@nafinc.com>
2025-04-23 10:23:05 -0500

    Add centering for scrolling.

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -4,6 +4,10 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
+-- Basic navigation
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
